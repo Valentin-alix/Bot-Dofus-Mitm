a8caa032b98e1f3655e5adf6c7b814cec3b9ff38
Valentin-alix <valentin-alix.850@outlook.fr>
2022-04-01 23:12:26 +0200

    commit

--- decompiler.bat
+++ decompiler.bat
@@ -1,5 +1,5 @@
 set ffDecJar="C:\Program Files (x86)\FFDec\ffdec.jar"
 set dofInvoker="C:\Users\valen\AppData\Local\Ankama\Dofus\DofusInvoker.swf"
-set pathDecompile="C:\Users\valen\Documents\Workspace\Python\Bot_Sniffer_Click\decompiled_scripts"
+set pathDecompile="C:\Users\valen\Documents\Workspace\Python\bot_mitm\decompiled_scripts"
 
 java -jar %ffDecJar% -selectclass com.ankamagames.dofus.network.messages.++,com.ankamagames.dofus.network.types.++,com.ankamagames.dofus.network.ProtocolTypeManager,com.ankamagames.dofus.network.MessageReceiver -export script %pathDecompile% %dofInvoker%
