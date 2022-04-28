f23dd61fe01ea2b0fde3dabc0d1c39cbd9fb4aae
Valentin-alix <valentin-alix.850@outlook.fr>
2022-04-29 01:26:52 +0200

    complete kama/tenta + FIX SNIFFER bug with lost segment

--- bot/network/sniffer.py
+++ bot/network/sniffer.py
@@ -58,11 +58,31 @@ class Sniffer:
             try:
                 logging.info(f"Received_Packet : {packet.data.data}")
                 if hasattr(packet[packet.transport_layer], 'analysis_lost_segment'):
+                    logging.info("erreur lost segment")
                     self.temp_buffer = bytearray.fromhex(packet.data.data)
                     if packet.ip.src == self.IP_LOCALE:
                         self.temp_buffer_from_local = True
+                        continue
                     else:
                         self.temp_buffer_from_local = False
+                        continue
+
+                try:
+                    if hasattr(packet.tcp, 'segment_data'):
+                        if packet.ip.src == self.IP_LOCALE:
+                            self.buffer_client += str(packet.tcp.segment_data).replace(":", "")
+                            if len(self.temp_buffer) != 0 and self.temp_buffer_from_local:
+                                self.buffer_client += self.temp_buffer
+                                self.temp_buffer.__init__()
+                            self.on_receive(self.buffer_client, True)
+                        else:
+                            self.buffer_server += str(packet.tcp.segment_data).replace(":", "")
+                            if len(self.temp_buffer) != 0 and not self.temp_buffer_from_local:
+                                self.buffer_server += self.temp_buffer
+                                self.temp_buffer.__init__()
+                            self.on_receive(self.buffer_server, False)
+                except AttributeError:
+                    pass
 
                 if packet.ip.src == self.IP_LOCALE:
                     self.buffer_client += bytearray.fromhex(packet.data.data)
