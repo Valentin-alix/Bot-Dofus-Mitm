987908f6afb18a4a060423c6350d58d7cd3c0115
Valentin-alix <valentin-alix.850@outlook.fr>
2022-04-29 21:51:35 +0200

    update

---
+++ bot/tests/sniffer_tests.py
@@ -0,0 +1,17 @@
+import asyncio
+import unittest
+
+import pyshark
+
+from bot.network.sniffer import Sniffer
+
+
+class TestSniffer(unittest.TestCase):
+    def setUp(self) -> None:
+        self.file = "test_capture.pcap"
+
+    def test_no_exception(self):
+        capture = pyshark.FileCapture(f"ressources/{self.file}", display_filter=Sniffer.FILTER_DOFUS)
+        for packet in capture:
+            print(packet)
+        capture.close()
--- setup/decompiler.bat
+++ setup/decompiler.bat
@@ -1,5 +1,5 @@
 set ffDecJar="C:\Program Files (x86)\FFDec\ffdec.jar"
 set dofInvoker="C:\Users\valen\AppData\Local\Ankama\Dofus\DofusInvoker.swf"
-set pathDecompile="./scripts_resources"
+set pathDecompile="../static/scripts_resources"
 
 java -jar %ffDecJar% -selectclass com.ankamagames.dofus.network.messages.++,com.ankamagames.dofus.network.types.++,com.ankamagames.dofus.network.ProtocolTypeManager,com.ankamagames.dofus.network.MessageReceiver -export script %pathDecompile% %dofInvoker%
