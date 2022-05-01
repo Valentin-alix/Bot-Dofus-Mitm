6bb63c8d6cbc98b61c12c812c69317d06c11a997
Valentin-alix <valentin-alix.850@outlook.fr>
2022-05-01 13:50:15 +0200

    update sniffer

--- bot/bot_fm.py
+++ bot/bot_fm.py
@@ -2,20 +2,21 @@ import datetime
 import logging
 import threading
 
-from gui.interface import Interface
+from bot.gui import interface
+from bot.gui.interface import Interface
 from network.sniffer import Sniffer
 
 if __name__ == '__main__':
     executed_time = str(datetime.datetime.now()).replace(':', '.')
     logging.basicConfig(filename=f"../logs/{str(executed_time)}.log", level=logging.INFO)
 
-    interface = Interface()
+    interface.interface = Interface()
     sniffer = Sniffer()
 
-    interface_thread = threading.Thread(target=interface.window)
+    interface_thread = threading.Thread(target=interface.interface.window)
     sniffer_thread = threading.Thread(target=sniffer.launch_sniffer)
 
     interface_thread.start()
     sniffer_thread.start()
 
-    interface.root.mainloop()
+    interface.interface.root.mainloop()
